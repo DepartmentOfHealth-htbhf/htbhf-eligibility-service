2d00927ec1765c04ed7b2f6663f5e6fd27f4edce
Matthew Smith <matthew.smith@thinkinglogic.com>
2018-11-22 09:01:54 +0000

    Added spock unit testing framework

--- src/main/java/uk/gov/dhsc/htbhf/eligibility/controller/EligibilityController.java
+++ src/main/java/uk/gov/dhsc/htbhf/eligibility/controller/EligibilityController.java
@@ -3,7 +3,10 @@ package uk.gov.dhsc.htbhf.eligibility.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import uk.gov.dhsc.htbhf.eligibility.model.Decision;
 import uk.gov.dhsc.htbhf.eligibility.model.EligibilityResponse;
+
+import static uk.gov.dhsc.htbhf.eligibility.model.Decision.ELIGIBLE;
 
 @Controller
 public class EligibilityController {
@@ -11,7 +14,9 @@ public class EligibilityController {
     @GetMapping("/eligibility")
     @ResponseBody
     public EligibilityResponse getDecision() {
-        return EligibilityResponse.builder().build();
+        return EligibilityResponse.builder()
+                .decision(ELIGIBLE)
+                .build();
     }
 
 }
---
+++ src/test/groovy/eligibility/controller/EligibilityControllerTest.groovy
@@ -0,0 +1,21 @@
+package eligibility.controller
+
+
+import spock.lang.Specification
+import uk.gov.dhsc.htbhf.eligibility.controller.EligibilityController
+import uk.gov.dhsc.htbhf.eligibility.model.EligibilityResponse
+
+import static uk.gov.dhsc.htbhf.eligibility.model.Decision.ELIGIBLE
+
+class EligibilityControllerTest extends Specification {
+    EligibilityController eligibilityController = new EligibilityController()
+
+    def "empty request should be eligible"() {
+        when:
+        EligibilityResponse result = eligibilityController.getDecision()
+
+        then:
+        result != null
+        result.decision == ELIGIBLE
+    }
+}
