d3a5513f17532b2237984eb1ad453efe45df7b27
David Wybourn <dwybourn@users.noreply.github.com>
2018-12-05 15:16:00 +0000

    Feature/blue green refactor (#13)
    * HTBHFB-4 Environment variables are now checked at the beginning of the deployment script and blue green deployment steps have been refactored.

--- ci_scripts/deploy.sh
+++ ci_scripts/deploy.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+perform_blue_green_deployment() {
+  echo "$APP_FULL_NAME exists, performing blue-green deployment"
+
+  cf push -p ${APP_PATH} --var suffix=${CF_SPACE}-green
+  cf map-route ${APP_FULL_NAME}-green ${CF_DOMAIN} --hostname ${APP_FULL_NAME}
+  unmap_blue_route
+  unmap_temporary_route
+  cf delete -f ${APP_FULL_NAME}
+  cf rename ${APP_FULL_NAME}-green ${APP_FULL_NAME}
+}
+
+unmap_blue_route() {
+  if cf check-route ${APP_FULL_NAME} ${CF_DOMAIN}; then
+    cf unmap-route ${APP_FULL_NAME} ${CF_DOMAIN} --hostname ${APP_FULL_NAME}
+  fi
+}
+
+unmap_temporary_route() {
+  if cf check-route ${APP_FULL_NAME}-green ${CF_DOMAIN}; then
+    cf unmap-route ${APP_FULL_NAME}-green ${CF_DOMAIN} --hostname ${APP_FULL_NAME}-green
+    cf delete-route -f ${CF_DOMAIN} --hostname ${APP_FULL_NAME}-green
+  fi
+}
+
 # quit at first error
 set -e
 
@@ -11,6 +35,20 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false"  || "$TRAVIS_BRANCH" != "master" ]]; the
    echo "Not tagging pull request or branch build"
    exit
 fi
+
+check_variable_is_set(){
+    if [[ -z ${!1} ]]; then
+        echo "$1 must be set and non empty"
+        exit 1
+    fi
+}
+ # check necessary environment variables are set and not empty
+check_variable_is_set CF_SPACE
+check_variable_is_set CF_API
+check_variable_is_set CF_ORG
+check_variable_is_set CF_USER
+check_variable_is_set CF_PASS
+check_variable_is_set CF_DOMAIN
 
 /bin/bash ci_scripts/install_cf_cli.sh;
 
@@ -25,16 +63,7 @@ APP_VERSION=`cat version.properties | grep "version" | cut -d'=' -f2`
 APP_PATH="build/libs/$APP_NAME-$APP_VERSION.jar"
 
 if cf app ${APP_FULL_NAME} >/dev/null 2>/dev/null; then
-  echo "$APP_FULL_NAME exists, performing blue-green deployment"
-
-  cf push -p ${APP_PATH} --var suffix=${CF_SPACE}-green
-  cf map-route ${APP_FULL_NAME}-green ${CF_DOMAIN} --hostname ${APP_FULL_NAME}
-  cf unmap-route ${APP_FULL_NAME} ${CF_DOMAIN} --hostname ${APP_FULL_NAME}
-  cf unmap-route ${APP_FULL_NAME}-green ${CF_DOMAIN} --hostname ${APP_FULL_NAME}-green
-  cf delete-route -f ${CF_DOMAIN} --hostname ${APP_FULL_NAME}-green
-  cf delete -f ${APP_FULL_NAME}
-  cf rename ${APP_FULL_NAME}-green ${APP_FULL_NAME}
-
+  perform_blue_green_deployment
 else
   echo "$APP_FULL_NAME does not exist, doing regular deployment"
   cf push -p ${APP_PATH} --var suffix=${CF_SPACE}
