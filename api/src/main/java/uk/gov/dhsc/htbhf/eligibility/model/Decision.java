20347cce6a41781b951093745431c4514cbfc51f
Matthew Smith <msmith@equalexperts.com>
2018-12-07 15:51:42 +0000

    HTBHFB-23 removed script version from .travis.yml, added javadoc to eligibility decision

--- api/src/main/java/uk/gov/dhsc/htbhf/eligibility/model/Decision.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/eligibility/model/Decision.java
@@ -1,5 +1,8 @@
 package uk.gov.dhsc.htbhf.eligibility.model;
 
+/**
+ * Represents a decision on the applications eligibility for help to buy healthy foods.
+ */
 public enum Decision {
     ELIGIBLE,
     INELIGIBLE;
