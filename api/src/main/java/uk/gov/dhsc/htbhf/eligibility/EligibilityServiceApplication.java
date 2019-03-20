46e2ad05c2a49bd3293d60220403f7a3a04f5ccc
David Wybourn <david.wybourn@gmail.com>
2019-03-20 10:11:07 +0000

    HTBHF-632 Enabling swagger doc and added logging config.

--- api/src/main/java/uk/gov/dhsc/htbhf/eligibility/EligibilityServiceApplication.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/eligibility/EligibilityServiceApplication.java
@@ -3,10 +3,12 @@ package uk.gov.dhsc.htbhf.eligibility;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import uk.gov.dhsc.htbhf.CommonRestConfiguration;
 
 @SuppressWarnings("PMD.UseUtilityClass")
 @SpringBootApplication
+@EnableSwagger2
 @Import(CommonRestConfiguration.class)
 public class EligibilityServiceApplication {
 
---
+++ api/src/test/java/uk/gov/dhsc/htbhf/eligibility/EligibilityServiceApplicationTests.java
@@ -0,0 +1,57 @@
+package uk.gov.dhsc.htbhf.eligibility;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import com.google.common.io.Files;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = EligibilityServiceApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class EligibilityServiceApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    void swaggerDocumentationRetrieved() throws IOException {
+        String response = testRestTemplate.getForObject(buildUrl(), String.class);
+
+        String jsonAsYaml = convertJsonToYaml(response);
+
+        assertThat(jsonAsYaml).isNotBlank();
+        assertThat(jsonAsYaml).containsSequence("swagger: \"2.0\"");
+
+        File swaggerFile = new File("../swagger.yml");
+        Files.asCharSink(swaggerFile, Charset.defaultCharset()).write(jsonAsYaml);
+    }
+
+    private String convertJsonToYaml(String response) throws IOException {
+        JsonNode jsonNodeTree = new ObjectMapper().readTree(response);
+        return new YAMLMapper()
+                .disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER)
+                .writeValueAsString(jsonNodeTree);
+    }
+
+    private String buildUrl() {
+        return "http://localhost:" + port + "/v2/api-docs";
+    }
+
+}
