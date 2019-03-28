891b4add84567d86422079a1c5e4b4c343ef3f59
Mike Smith <mike.smith@sescon.co.uk>
2019-03-28 14:14:47 +0000

    HTBHF-633 Use common code for integration test assertions and swagger generation.

--- api/src/test/java/uk/gov/dhsc/htbhf/eligibility/controller/EligibilityControllerTest.java
+++ api/src/test/java/uk/gov/dhsc/htbhf/eligibility/controller/EligibilityControllerTest.java
@@ -17,16 +17,10 @@ import uk.gov.dhsc.htbhf.errorhandler.ErrorResponse;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.OK;
+import static uk.gov.dhsc.htbhf.assertions.IntegrationTestAssertions.assertValidationErrorInResponse;
 import static uk.gov.dhsc.htbhf.eligibility.helper.EligibilityResponseTestFactory.anEligibilityResponse;
-import static uk.gov.dhsc.htbhf.eligibility.helper.PersonDTOTestFactory.aPerson;
-import static uk.gov.dhsc.htbhf.eligibility.helper.PersonDTOTestFactory.aPersonWithDateOfBirthInFuture;
-import static uk.gov.dhsc.htbhf.eligibility.helper.PersonDTOTestFactory.aPersonWithNoAddress;
-import static uk.gov.dhsc.htbhf.eligibility.helper.PersonDTOTestFactory.aPersonWithNoDateOfBirth;
-import static uk.gov.dhsc.htbhf.eligibility.helper.PersonDTOTestFactory.aPersonWithNoNino;
-import static uk.gov.dhsc.htbhf.eligibility.helper.PersonDTOTestFactory.aPersonWithPostcode;
-import static uk.gov.dhsc.htbhf.eligibility.helper.PersonDTOTestFactory.buildDefaultPerson;
+import static uk.gov.dhsc.htbhf.eligibility.helper.PersonDTOTestFactory.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -57,9 +51,9 @@ class EligibilityControllerTest {
     void shouldReturnBadRequestForMissingNino() {
         PersonDTO person = aPersonWithNoNino();
 
-        var response = restTemplate.postForEntity(ENDPOINT_URL, person, ErrorResponse.class);
+        ResponseEntity<ErrorResponse> response = restTemplate.postForEntity(ENDPOINT_URL, person, ErrorResponse.class);
 
-        assertBadRequest(response, "nino", "must not be null");
+        assertValidationErrorInResponse(response, "nino", "must not be null");
     }
 
     @ParameterizedTest
@@ -67,36 +61,36 @@ class EligibilityControllerTest {
     void shouldReturnBadRequestForInvalidNino(String nino) {
         PersonDTO person = buildDefaultPerson().nino(nino).build();
 
-        var response = restTemplate.postForEntity(ENDPOINT_URL, person, ErrorResponse.class);
+        ResponseEntity<ErrorResponse> response = restTemplate.postForEntity(ENDPOINT_URL, person, ErrorResponse.class);
 
-        assertBadRequest(response, "nino", "must match \"[a-zA-Z]{2}\\d{6}[a-dA-D]\"");
+        assertValidationErrorInResponse(response, "nino", "must match \"[a-zA-Z]{2}\\d{6}[a-dA-D]\"");
     }
 
     @Test
     void shouldReturnBadRequestForMissingDateOfBirth() {
         PersonDTO person = aPersonWithNoDateOfBirth();
 
-        var response = restTemplate.postForEntity(ENDPOINT_URL, person, ErrorResponse.class);
+        ResponseEntity<ErrorResponse> response = restTemplate.postForEntity(ENDPOINT_URL, person, ErrorResponse.class);
 
-        assertBadRequest(response, "dateOfBirth", "must not be null");
+        assertValidationErrorInResponse(response, "dateOfBirth", "must not be null");
     }
 
     @Test
     void shouldReturnBadRequestForInvalidDateOfBirth() {
         PersonDTO person = aPersonWithDateOfBirthInFuture();
 
-        var response = restTemplate.postForEntity(ENDPOINT_URL, person, ErrorResponse.class);
+        ResponseEntity<ErrorResponse> response = restTemplate.postForEntity(ENDPOINT_URL, person, ErrorResponse.class);
 
-        assertBadRequest(response, "dateOfBirth", "must be a past date");
+        assertValidationErrorInResponse(response, "dateOfBirth", "must be a past date");
     }
 
     @Test
     void shouldReturnBadRequestForMissingAddress() {
         PersonDTO person = aPersonWithNoAddress();
 
-        var response = restTemplate.postForEntity(ENDPOINT_URL, person, ErrorResponse.class);
+        ResponseEntity<ErrorResponse> response = restTemplate.postForEntity(ENDPOINT_URL, person, ErrorResponse.class);
 
-        assertBadRequest(response, "address", "must not be null");
+        assertValidationErrorInResponse(response, "address", "must not be null");
     }
 
     @ParameterizedTest
@@ -104,18 +98,9 @@ class EligibilityControllerTest {
     void shouldReturnBadRequestForInvalidPostcode(String postcode) {
         PersonDTO person = aPersonWithPostcode(postcode);
 
-        var response = restTemplate.postForEntity(ENDPOINT_URL, person, ErrorResponse.class);
+        ResponseEntity<ErrorResponse> response = restTemplate.postForEntity(ENDPOINT_URL, person, ErrorResponse.class);
 
-        assertBadRequest(response, "address.postcode", "invalid postcode format");
+        assertValidationErrorInResponse(response, "address.postcode", "invalid postcode format");
     }
 
-    private void assertBadRequest(ResponseEntity<ErrorResponse> response, String field, String message) {
-        assertThat(response.getStatusCode()).isEqualTo(BAD_REQUEST);
-        assertThat(response.getBody()).isNotNull();
-        assertThat(response.getBody().getFieldErrors().size()).isEqualTo(1);
-        assertThat(response.getBody().getFieldErrors().get(0).getField()).isEqualTo(field);
-        assertThat(response.getBody().getFieldErrors().get(0).getMessage()).isEqualTo(message);
-        assertThat(response.getBody().getRequestId()).isNotNull();
-        assertThat(response.getBody().getTimestamp()).isNotNull();
-    }
 }
